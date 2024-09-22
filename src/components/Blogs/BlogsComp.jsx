010846c8f7966a2a6d1595299b44eae3ace047b5
Topinkilaz <juan.p.t.1234@gmail.com>
2024-09-22 19:45:06 -0400

    noticesUpdate

--- src/components/Blogs/BlogsComp.jsx
+++ src/components/Blogs/BlogsComp.jsx
@@ -1,51 +1,44 @@
 import React from "react";
-
 import { BlogCard } from "./BlogCard";
 import { useFetch } from "../../Hooks/useFetch";
 
+export const BlogsComp = ({ limit }) => {
+  const today = new Date();
+  today.setDate(today.getDate() - 1);  
+  const formattedYesterday = today.toISOString().split('T')[0]; 
+  
+  const access_key = '7fff07b166ee037ea5129f1472c32370';  
+  const category = 'medicina';  
+  const language = 'es';  
 
-export const BlogsComp = ({limit}) => {
-
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
-  const formattedYesterday = yesterday.toISOString().split('T')[0];
+  
+  const { data } = useFetch(`https://api.mediastack.com/v1/news?access_key=${access_key}&languages=${language}&keywords=${category}&date=${formattedYesterday}`);
 
 
-  const key= '3f99b611d2724800a9b792b1ea48609c';
-  const value= 'enfermedades';
-  const {data}=useFetch(`https://newsapi.org/v2/everything?q=${value}&from=${formattedYesterday}&sortBy=relevancy&language=es&apiKey=${key}`)
 
- 
   return (
     <>
-     <div className="dark:bg-gray-900 dark:text-white py-10 font-montserrat">
-        <section data-aos="fade-up" className="container ">
-          
+      <div className="dark:bg-gray-900 dark:text-white py-10 font-montserrat">
+        <section data-aos="fade-up" className="container">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
-          {data && data.articles ? (
-            
-        data.articles.slice(0, limit).map((article, index) => (
-          <BlogCard
-          key={index}
-          image={article.urlToImage}
-          date={article.publishedAt}
-          title={article.title}
-          description={article.description} 
-          author={article.author}
-          url={article.url}
-             
-       
-          />
-        ))
-        
-      ) : (
-        <p>Sin articulos publicados</p>
-      )}
+            {data && data.data ? (  // Verifica si 'data.data' existe y tiene artículos
+              data.data.slice(0, limit).map((article, index) => (
+                <BlogCard
+                  key={index}
+                  image={article.image}  // Usa 'image' para la URL de la imagen
+                  date={article.published_at}  // Usa 'published_at' para la fecha de publicación
+                  title={article.title}
+                  description={article.description}
+                  author={article.author}
+                  url={article.url}
+                />
+              ))
+            ) : (
+              <p>Sin artículos publicados</p>
+            )}
           </div>
         </section>
       </div>
     </>
-  )
-}
-
-
+  );
+};
